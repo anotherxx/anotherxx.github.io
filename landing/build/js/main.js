d5725c3882b5a243b84e2908597ae941ecd462ba
Nerhbest <nerhbest@gmail.com>
2018-08-12 03:53:33 +0300

    Pre

--- landing/build/js/main.js
+++ landing/build/js/main.js
@@ -1,42 +1,16 @@
-/*******************************************************
-					Reviews Carousel Module
-*******************************************************/
-(function($) {
-	$(".owl-carousel").owlCarousel({
-		loop: false,
-		margin: 10,
-		items: 1,
-		nav: false
+
+/*
+==========   NiceScroll   ==========
+*/
+(function ($) {
+	$("body").niceScroll({
+		cursorborder: false
 	});
 })(jQuery);
 
 /*
-==========   Map Address Module   ==========
-*/
-(function($) {
-
-	ymaps.ready(initMap);
-
-	function initMap()
-	{
-		var myMap = new ymaps.Map("address-map", {
-			center: [59.959012,30.313523],
-			zoom: 16
-		});
-
-		var myPlaceMark = new ymaps.Placemark([59.959012,30.313523], {
-			hintContent: "Расположение"
-		});
-
-		myMap.geoObjects.add(myPlaceMark);
-	}
-
-
-})(jQuery);
-/*
 ==========   Mobile Navigation   ==========
 */
-
 (function($) {
     var mobileNavigation = $('.mobile-navigation__links');
 
@@ -46,47 +20,23 @@
 })(jQuery);
 
 /*
-==========   Modals   ==========
-*/
-
-(function($) {
-	MicroModal.init();
-})(jQuery);
-
-/*
-==========   Smooth Scroll To Section   ==========
+==========   Wow js Init   ==========
 */
 (function($) {
-	var links = $(".navigation a");
-	var mobileLinks = $(".mobile-navigation a");
-	var doScroll = function(e) {
-        	var goToClass = "." + e.target.dataset.goTo;
-        	$('html, body').animate({
-            	scrollTop: $(goToClass).offset().top
-        	}, 500);
-	};
-	links.click(function(e) {
-		e.preventDefault();
-		doScroll(e);
-	});
-	mobileLinks.click(function(e){
-		e.preventDefault();
-		doScroll(e);
-		$(".mobile-navigation__links").removeClass('mobile-navigation__links--visible');
-	});
+    new WOW().init();
 })(jQuery);
 
 /*
 ==========   Fixed Navigation   ==========
 */
 (function($) {
-	var needestScrollHeight = $('.top-line').outerHeight(true);
-	var navigation = $(".navigation");
-	var isNavigationFixed = false;
+    var needestScrollHeight = $('.top-line').outerHeight(true);
+    var navigation = $(".navigation");
+    var isNavigationFixed = false;
     window.addEventListener('scroll', function(e){
         if(window.pageYOffset > needestScrollHeight && !isNavigationFixed){
-        	navigation.addClass('navigation--fixed');
-        	isNavigationFixed = true;
+            navigation.addClass('navigation--fixed');
+            isNavigationFixed = true;
         }
 
         if(window.pageYOffset < needestScrollHeight && isNavigationFixed){
@@ -97,5 +47,70 @@
 })(jQuery);
 
 
+/*
+==========   Smooth Scroll To Section   ==========
+*/
+(function($) {
+    var links = $(".navigation a");
+    var mobileLinks = $(".mobile-navigation a");
+    var doScroll = function(e) {
+        var goToClass = "." + e.target.dataset.goTo;
+        $('html, body').animate({
+            scrollTop: $(goToClass).offset().top
+        }, 500);
+    };
+    links.click(function(e) {
+        e.preventDefault();
+        doScroll(e);
+    });
+    mobileLinks.click(function(e){
+        e.preventDefault();
+        doScroll(e);
+        $(".mobile-navigation__links").removeClass('mobile-navigation__links--visible');
+    });
+})(jQuery);
+
+/*
+==========   Modals   ==========
+*/
+
+(function($) {
+	MicroModal.init();
+})(jQuery);
 
 
+/*
+==========   Owl Carousel Init   ==========
+*/
+(function($) {
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        items: 1,
+        nav: false
+    });
+})(jQuery);
+
+/*
+==========   Map Adress Module   ==========
+*/
+(function($) {
+
+    ymaps.ready(initMap);
+
+    function initMap()
+    {
+        var myMap = new ymaps.Map("address-map", {
+            center: [59.959012,30.313523],
+            zoom: 16
+        });
+
+        var myPlaceMark = new ymaps.Placemark([59.959012,30.313523], {
+            hintContent: "Расположение"
+        });
+
+        myMap.geoObjects.add(myPlaceMark);
+    }
+
+
+})(jQuery);
